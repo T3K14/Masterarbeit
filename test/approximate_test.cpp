1fad4d53392e9df930bafdac085262190dbda655
T3K14 <robert97752@gmail.com>
2021-10-26 14:49:28 +0200

    alles umbenannt, damit nicht alls mehrfach deklariert wird

--- test/approximate_test.cpp
+++ test/approximate_test.cpp
@@ -20,23 +20,23 @@ int main() {
 
     try {
         // test 1
-        unsigned int numberScenarios = 1;
-        unsigned int numberNodes = 2;
+        unsigned int numberScenarios0 = 1;
+        unsigned int numberNodes0 = 2;
 
     // std::array<double, 3> scenarioProbabilities {0.2, 0.2, 0.6};
     // std::array<double, 6> firstStageWeights {0.5, 1., 10., 10., 1., 1.};
     // std::array<std::array<double, 6>, 3> secondStageWeights = {{{1.5, 1.5, 1.5, 1.5, 1.5, 1.5}, {1.5, 1.5, 1., 2., 1.5, 1.5}, {0.5, 1.5, 1.5, 0.5, 1., 0.5}}};
     
-        std::vector<double> scenarioProbabilities {1.0};
-        std::vector<double> firstStageWeights {1.5};
-        std::vector<std::vector<double>> secondStageWeights {{{0.5}}};
+        std::vector<double> scenarioProbabilities0 {1.0};
+        std::vector<double> firstStageWeights0 {1.5};
+        std::vector<std::vector<double>> secondStageWeights0 {{{0.5}}};
 
-        FullyConnectedTwoStageMST mst(numberNodes, firstStageWeights, secondStageWeights, scenarioProbabilities);
+        FullyConnectedTwoStageMST mst0(numberNodes0, firstStageWeights0, secondStageWeights0, scenarioProbabilities0);
 
         //std::cout << "Komme ich bis hier?\n";
 
-        solve_relaxed_lp(mst);
-        mst.save_lp_result_map("first");
+        solve_relaxed_lp(mst0);
+        mst0.save_lp_result_map("first");
 
 
 
