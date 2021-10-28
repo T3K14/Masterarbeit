08624560f875eeb118b3e5f66df37fa40cffff98
T3K14 <robert97752@gmail.com>
2021-10-28 21:06:48 +0200

    jetzt 10 statt 6 knoten, da wuerde ich denken, dass die bruteforcemethode sehr lange braucht

--- simulationen/bruteforce.cpp
+++ simulationen/bruteforce.cpp
@@ -6,16 +6,20 @@
 int main() {
 
     // Problemdefinition
-
+    // auskommentiert: 6 Knoten
     std::vector<double> scenarioProbabilities {0.2, 0.3, 0.5};
-    std::vector<double> firstStageWeights {0.5, 1., 10., 10., 1., 1., 6., 8.5, 0.6, 1.2, 1.1, 15., 2., 0.1, 0.2};
-    std::vector<std::vector<double>> secondStageWeights {{{2.5, 1.8, 10.5, 1.2, 0.05, 12.2, 6., 8.5, 1.6, 3.2, 0.1, 15., 2.25, 0.6, 0.1}, {19., 90., 5., 18., 20., 12.73, 3.14, 8.5, 9.6, 3.1, 1.1, 3.7, 2., 0.6, 2.2}, {0.5, 1., 3., 10., 1.6, 1., 0.6, 0.5, 14.6, 1.2, 3.2, 1.3, 0.02, 0.1, 5.2}}};
+    // std::vector<double> firstStageWeights {0.5, 1., 10., 10., 1., 1., 6., 8.5, 0.6, 1.2, 1.1, 15., 2., 0.1, 0.2};
+    // std::vector<std::vector<double>> secondStageWeights {{{2.5, 1.8, 10.5, 1.2, 0.05, 12.2, 6., 8.5, 1.6, 3.2, 0.1, 15., 2.25, 0.6, 0.1}, {19., 90., 5., 18., 20., 12.73, 3.14, 8.5, 9.6, 3.1, 1.1, 3.7, 2., 0.6, 2.2}, {0.5, 1., 3., 10., 1.6, 1., 0.6, 0.5, 14.6, 1.2, 3.2, 1.3, 0.02, 0.1, 5.2}}};
+    std::vector<double> firstStageWeights {0.5, 1., 10., 10., 1., 1., 6., 8.5, 0.6, 1.2, 1.1, 15., 2., 0.1, 0.2, 0.5, 1., 10., 10., 1., 1., 6., 8.5, 0.6, 1.2, 1.1, 15., 2., 0.1, 0.2, 0.5, 1., 10., 10., 1., 1., 6., 8.5, 0.6, 1.2, 1.1, 15., 2., 0.1, 0.2};
+    std::vector<std::vector<double>> secondStageWeights {{{2.5, 1.8, 10.5, 1.2, 0.05, 12.2, 6., 8.5, 1.6, 3.2, 0.1, 15., 2.25, 0.6, 0.1}, {19., 90., 5., 18., 20., 12.73, 3.14, 8.5, 9.6, 3.1, 1.1, 3.7, 2., 0.6, 2.2}, {0.5, 1., 3., 10., 1.6, 1., 0.6, 0.5, 14.6, 1.2, 3.2, 1.3, 0.02, 0.1, 5.2}, {2.5, 1.8, 10.5, 1.2, 0.05, 12.2, 6., 8.5, 1.6, 3.2, 0.1, 15., 2.25, 0.6, 0.1}, {19., 90., 5., 18., 20., 12.73, 3.14, 8.5, 9.6, 3.1, 1.1, 3.7, 2., 0.6, 2.2}, {0.5, 1., 3., 10., 1.6, 1., 0.6, 0.5, 14.6, 1.2, 3.2, 1.3, 0.02, 0.1, 5.2}}};
 
-    FullyConnectedTwoStageMST mst(6, firstStageWeights, secondStageWeights, scenarioProbabilities);
+
+
+    FullyConnectedTwoStageMST mst(10, firstStageWeights, secondStageWeights, scenarioProbabilities);
 
     // auto res = bruteForceEnumeration(mst.g, mst.firstStageWeights, mst.secondStageProbabilities, mst.secondStageWeights);
     auto res = mst.bruteforce();
-    mst.save_bruteforce_first_stage_map("bruteforce_erste_simulation");
+    mst.save_bruteforce_first_stage_map("bruteforce_zweite_simulation");
 
     return 0;
 }
