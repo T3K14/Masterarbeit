efa178390ceb2ff71130c7ddce3837edbb9fe61b
T3K14 <robert97752@gmail.com>
2021-12-01 21:12:47 +0100

    vergessen, das new zu entfernen

--- simulationen/bruteforce.cpp
+++ simulationen/bruteforce.cpp
@@ -22,7 +22,7 @@ int main() {
     FullyConnectedTwoStageMST mst(10, firstStageWeights, secondStageWeights, scenarioProbabilities);
 
     // auto res = bruteForceEnumeration(mst.g, mst.firstStageWeights, mst.secondStageProbabilities, mst.secondStageWeights);
-    auto res = mst.bruteforce_new();
+    auto res = mst.bruteforce();
     mst.save_bruteforce_first_stage_map("bruteforce_dritte_simulation");
 
     double expected_costs = mst.calculate_expected_from_bool_map(mst.bruteforce_first_stage_map);
