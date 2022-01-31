49a4827acef62ee377902b63cfc92dea7093718b
T3K14 <robert97752@gmail.com>
2022-01-31 18:03:02 +0100

    ohne while

--- simulationen/approx_time_test.cpp
+++ simulationen/approx_time_test.cpp
@@ -65,146 +65,146 @@ int main(int argc, char * argv[]) {
     // hier speicher ich die Zeiten, wie lange alle Optimierungen bei allen Iteratioenn gedauert haben
     // std::vector<std::vector<std::chrono::milliseconds>> optimierungs_zeiten;
 
-    try {
+    // try {
        
-        // std::cout << "Nodes: " << number_nodes << "\n";
-        // std::cout << "Iterationen: " << iterations << "\n";
-        // std::cout << "Anzahl Edges: " << number_edges << "\n";
+    //     // std::cout << "Nodes: " << number_nodes << "\n";
+    //     // std::cout << "Iterationen: " << iterations << "\n";
+    //     // std::cout << "Anzahl Edges: " << number_edges << "\n";
 
-        // loope ueber Anzahl Iterationen und fuere so oft ein fully connected Problem aus und tracke die Dauer und die Anzahl an hinzugefuegten Bedingungen
+    //     // loope ueber Anzahl Iterationen und fuere so oft ein fully connected Problem aus und tracke die Dauer und die Anzahl an hinzugefuegten Bedingungen
 
-        // counter
-        ofstream counter_file;
-        std::string counter_path = outputPath + "/counters.txt";
+    //     // counter
+    //     ofstream counter_file;
+    //     std::string counter_path = outputPath + "/counters.txt";
 
-        // setup zeit
-        ofstream setup_file;
-        std::string setup_path = outputPath + "/setup_zeiten_s.txt";
+    //     // setup zeit
+    //     ofstream setup_file;
+    //     std::string setup_path = outputPath + "/setup_zeiten_s.txt";
 
-        // loop zeit
-        ofstream loop_file;
-        std::string loop_path = outputPath + "/loop_zeiten_s.txt";
+    //     // loop zeit
+    //     ofstream loop_file;
+    //     std::string loop_path = outputPath + "/loop_zeiten_s.txt";
 
-        // die optimierungszeiten
-        std::string opt_path = outputPath + "/opt/";
+    //     // die optimierungszeiten
+    //     std::string opt_path = outputPath + "/opt/";
 
-        int counter_iteration = 0;
+    //     int counter_iteration = 0;
 
-        NRandomScenarioCreator s(number_scenarios, rng);    
-        RandomTestCreator n(0., 10., rng);
-        FullyConnectedMinusEdges ensemble(number_nodes, s, n, rng, number_minus_edges);
+    //     NRandomScenarioCreator s(number_scenarios, rng);    
+    //     RandomTestCreator n(0., 10., rng);
+    //     FullyConnectedMinusEdges ensemble(number_nodes, s, n, rng, number_minus_edges);
 
-        while (true) {
+    //     while (true) {
         
-            unsigned long counter = 0;
-            std::chrono::seconds setup_zeit;
-            std::chrono::seconds loop_zeit;
+    //         unsigned long counter = 0;
+    //         std::chrono::seconds setup_zeit;
+    //         std::chrono::seconds loop_zeit;
 
-            std::vector<std::chrono::milliseconds> opt_times;
+    //         std::vector<std::chrono::milliseconds> opt_times;
 
-            double res = solve_relaxed_lp(ensemble.two_stage_problem, counter, setup_zeit, loop_zeit, opt_times);
+    //         double res = solve_relaxed_lp(ensemble.two_stage_problem, counter, setup_zeit, loop_zeit, opt_times);
 
-            // Ergebnisse abspeichern:
+    //         // Ergebnisse abspeichern:
 
-            counter_file.open(counter_path, std::ios::app);
-            counter_file << counter << "\n";
-            counter_file.close();
+    //         counter_file.open(counter_path, std::ios::app);
+    //         counter_file << counter << "\n";
+    //         counter_file.close();
 
-            setup_file.open(setup_path, std::ios::app);
-            setup_file << setup_zeit.count() << "\n";
-            setup_file.close();
+    //         setup_file.open(setup_path, std::ios::app);
+    //         setup_file << setup_zeit.count() << "\n";
+    //         setup_file.close();
 
-            loop_file.open (loop_path, std::ios::app);
-            loop_file << loop_zeit.count() << "\n";
-            loop_file.close();
+    //         loop_file.open (loop_path, std::ios::app);
+    //         loop_file << loop_zeit.count() << "\n";
+    //         loop_file.close();
 
-            ofstream opt_file;
-            opt_file.open(opt_path + std::to_string(counter_iteration) + "_ms.txt");
+    //         ofstream opt_file;
+    //         opt_file.open(opt_path + std::to_string(counter_iteration) + "_ms.txt");
 
-            for (auto t : opt_times) {
-                opt_file << t.count() << "\n";
-            }
+    //         for (auto t : opt_times) {
+    //             opt_file << t.count() << "\n";
+    //         }
 
-            opt_file.close();
+    //         opt_file.close();
         
-            // naechste Iteration
-            counter_iteration++;
-            ensemble.recreate();
+    //         // naechste Iteration
+    //         counter_iteration++;
+    //         ensemble.recreate();
 
-            // counters.push_back(counter);
-            // optimierungs_zeiten.push_back(opt_times);
-            // setup_zeiten.push_back(setup_zeit);
-            // loop_zeiten.push_back(loop_zeit);
+    //         // counters.push_back(counter);
+    //         // optimierungs_zeiten.push_back(opt_times);
+    //         // setup_zeiten.push_back(setup_zeit);
+    //         // loop_zeiten.push_back(loop_zeit);
 
-            //std::cout << "Ich komme bis zur Ausgabe des Ergebnisses des LP-Algorithmus\n" << std::endl;
-
-
-            // mst.approximate(rng);
-            // mst.save_approx_result_map("approx_long");
-
-            // double expected_costs = mst.calculate_expected_from_bool_map(mst.approx_first_stage_map);
-            // std::cout << "Mit dieser Auswahl hat man Gesamterwartungskosten von : " << expected_costs << std::endl;
-
-            // //output, zum Vergleichen
-            // std::cout << expected_costs << std::endl;
-
-        }
-        // alles abspeichern
-
-        // // counter
-        // ofstream counter_file;
-        // std::string counter_path = outputPath + "/counters.txt";
-
-        // std::cout << counter_path << "\n";
-
-        // counter_file.open (counter_path);
-
-        // for (auto c : counters) {
-        //     counter_file << c << "\n";
-        // }
-        // counter_file.close();
+    //         //std::cout << "Ich komme bis zur Ausgabe des Ergebnisses des LP-Algorithmus\n" << std::endl;
 
 
-        // // setup zeit
-        // ofstream setup_file;
-        // std::string setup_path = outputPath + "/setup_zeiten_s.txt";
+    //         // mst.approximate(rng);
+    //         // mst.save_approx_result_map("approx_long");
 
-        // setup_file.open (setup_path);
+    //         // double expected_costs = mst.calculate_expected_from_bool_map(mst.approx_first_stage_map);
+    //         // std::cout << "Mit dieser Auswahl hat man Gesamterwartungskosten von : " << expected_costs << std::endl;
 
-        // for (auto t : setup_zeiten) {
-        //     setup_file << t.count() << "\n";
-        // }
-        // setup_file.close();
+    //         // //output, zum Vergleichen
+    //         // std::cout << expected_costs << std::endl;
 
-        // // loop zeit
-        // ofstream loop_file;
-        // std::string loop_path = outputPath + "/loop_zeiten_s.txt";
+    //     }
+    //     // alles abspeichern
 
-        // loop_file.open (loop_path);
+    //     // // counter
+    //     // ofstream counter_file;
+    //     // std::string counter_path = outputPath + "/counters.txt";
 
-        // for (auto t : loop_zeiten) {
-        //     loop_file << t.count() << "\n";
-        // }
-        // loop_file.close();
+    //     // std::cout << counter_path << "\n";
+
+    //     // counter_file.open (counter_path);
+
+    //     // for (auto c : counters) {
+    //     //     counter_file << c << "\n";
+    //     // }
+    //     // counter_file.close();
 
 
-        // // die optimierungszeiten
-        // std::string opt_path = outputPath + "/opt/";
+    //     // // setup zeit
+    //     // ofstream setup_file;
+    //     // std::string setup_path = outputPath + "/setup_zeiten_s.txt";
 
-        // for (int i=0; i<iterations; i++) {
-        //     ofstream opt_file;
-        //     opt_file.open(opt_path + std::to_string(i) + "_ms.txt");
+    //     // setup_file.open (setup_path);
 
-        //     std::cout << opt_path + std::to_string(i) + "_ms.txt" << "\n";
+    //     // for (auto t : setup_zeiten) {
+    //     //     setup_file << t.count() << "\n";
+    //     // }
+    //     // setup_file.close();
 
-        //     for (auto t : optimierungs_zeiten[i]) {
-        //         opt_file << t.count() << "\n";
-        //     }
+    //     // // loop zeit
+    //     // ofstream loop_file;
+    //     // std::string loop_path = outputPath + "/loop_zeiten_s.txt";
 
-        //     opt_file.close();
-        // }
+    //     // loop_file.open (loop_path);
+
+    //     // for (auto t : loop_zeiten) {
+    //     //     loop_file << t.count() << "\n";
+    //     // }
+    //     // loop_file.close();
+
+
+    //     // // die optimierungszeiten
+    //     // std::string opt_path = outputPath + "/opt/";
+
+    //     // for (int i=0; i<iterations; i++) {
+    //     //     ofstream opt_file;
+    //     //     opt_file.open(opt_path + std::to_string(i) + "_ms.txt");
+
+    //     //     std::cout << opt_path + std::to_string(i) + "_ms.txt" << "\n";
+
+    //     //     for (auto t : optimierungs_zeiten[i]) {
+    //     //         opt_file << t.count() << "\n";
+    //     //     }
+
+    //     //     opt_file.close();
+    //     // }
         
-    }
+    // }
     // catch(GRBException e) {
     //     cout << "Error code = " << e.getErrorCode() << endl;
     //     cout << e.getMessage() << endl;
