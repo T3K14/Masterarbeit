8802b6eeeec215d6cd4c0118710557823d6e0333
T3K14 <robert97752@gmail.com>
2022-01-31 22:58:28 +0100

    nur 1 iteration

--- simulationen/approx_time_test.cpp
+++ simulationen/approx_time_test.cpp
@@ -51,7 +51,7 @@ int main(int argc, char * argv[]) {
     std::string outputPath = R"(/gss/work/xees8992/)";
     outputPath += directory;
 
-    std::cout << "ROBERTSAGTHIER\n" << std::endl;
+    // std::cout << "ROBERTSAGTHIER\n" << std::endl;
 
     // hier drin speichere ich pro run die Anzahl der hinzugefuegten Bedingungen
     // std::vector<unsigned long> counters;
@@ -64,8 +64,6 @@ int main(int argc, char * argv[]) {
     // hier speicher ich die Zeiten, wie lange alle Optimierungen bei allen Iteratioenn gedauert haben
     // std::vector<std::vector<std::chrono::milliseconds>> optimierungs_zeiten;
 
-}
-/*
     try {
        
         // std::cout << "Nodes: " << number_nodes << "\n";
@@ -95,42 +93,42 @@ int main(int argc, char * argv[]) {
         RandomTestCreator n(0., 10., rng);
         FullyConnectedMinusEdges ensemble(number_nodes, s, n, rng, number_minus_edges);
 
-        while (true) {
+        // while (true) {
         
-            unsigned long counter = 0;
-            std::chrono::seconds setup_zeit;
-            std::chrono::seconds loop_zeit;
+        unsigned long counter = 0;
+        std::chrono::seconds setup_zeit;
+        std::chrono::seconds loop_zeit;
 
-            std::vector<std::chrono::milliseconds> opt_times;
+        std::vector<std::chrono::milliseconds> opt_times;
 
-            double res = solve_relaxed_lp(ensemble.two_stage_problem, counter, setup_zeit, loop_zeit, opt_times);
+        double res = solve_relaxed_lp(ensemble.two_stage_problem, counter, setup_zeit, loop_zeit, opt_times);
 
-            // Ergebnisse abspeichern:
+        // Ergebnisse abspeichern:
 
-            counter_file.open(counter_path, std::ios::app);
-            counter_file << counter << "\n";
-            counter_file.close();
+        counter_file.open(counter_path, std::ios::app);
+        counter_file << counter << "\n";
+        counter_file.close();
 
-            setup_file.open(setup_path, std::ios::app);
-            setup_file << setup_zeit.count() << "\n";
-            setup_file.close();
+        setup_file.open(setup_path, std::ios::app);
+        setup_file << setup_zeit.count() << "\n";
+        setup_file.close();
 
-            loop_file.open (loop_path, std::ios::app);
-            loop_file << loop_zeit.count() << "\n";
-            loop_file.close();
+        loop_file.open (loop_path, std::ios::app);
+        loop_file << loop_zeit.count() << "\n";
+        loop_file.close();
 
-            ofstream opt_file;
-            opt_file.open(opt_path + std::to_string(counter_iteration) + "_ms.txt");
+        ofstream opt_file;
+        opt_file.open(opt_path + std::to_string(counter_iteration) + "_ms.txt");
 
-            for (auto t : opt_times) {
-                opt_file << t.count() << "\n";
-            }
+        for (auto t : opt_times) {
+            opt_file << t.count() << "\n";
+        }
 
-            opt_file.close();
-        
-            // naechste Iteration
-            counter_iteration++;
-            ensemble.recreate();
+        opt_file.close();
+    
+        // naechste Iteration
+        counter_iteration++;
+        ensemble.recreate();
 
             // counters.push_back(counter);
             // optimierungs_zeiten.push_back(opt_times);
@@ -149,7 +147,7 @@ int main(int argc, char * argv[]) {
             // //output, zum Vergleichen
             // std::cout << expected_costs << std::endl;
 
-        }
+        // }
         // alles abspeichern
 
         // // counter
@@ -216,4 +214,3 @@ int main(int argc, char * argv[]) {
 
     
 }
-*/
