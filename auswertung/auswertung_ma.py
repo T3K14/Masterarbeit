8c1be6b831550c4bacb2c6e97f673f3f07a62849
Robert Strassen <robert97752@gmail.com>
2022-07-07 13:59:55 +0200

    auto imports rausgenommen

--- auswertung/auswertung_ma.py
+++ auswertung/auswertung_ma.py
@@ -1,11 +1,8 @@
-from codecs import ignore_errors
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from shutil import move, rmtree
 import os
-
-from sklearn.neighbors import NearestCentroid
 
 def read_tracking_files_sim(p, read_check=False, read_opt=False, read_lp=False, read_constr=False):
     """Laedt Tracking Datein fuer einen simulation_x Ordner
