c30224a8482485bb499bf1cd98f498bdb9d9120f
Robert Strassen <robert97752@gmail.com>
2022-07-21 13:37:44 +0200

    war quatsch..

--- auswertung/auswertung_ma.py
+++ auswertung/auswertung_ma.py
@@ -486,7 +486,7 @@ class Read_HO:
             # print(self.id_tups)
 
             print('Lese die TrackingDaten ein...')
-            self.dfs = {id: read_tracking_files(os.path.join(self.path_ho, f), read_lp=False) for id, f in self.id_tups}      # hier stehen die tracking daten drin
+            self.dfs = {id: read_tracking_files(os.path.join(self.path_ho, f), read_lp=read_lp) for id, f in self.id_tups}      # hier stehen die tracking daten drin
             print('fertig!')
 
 
